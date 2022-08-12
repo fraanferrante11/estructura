2b9f9455eb0f89f8a885b0fe1f871c3cbb981264
fraanfe <fraanferrante11@gmail.com>
2022-08-12 09:31:52 -0300

    Create Ejercicio13.py

---
+++ Ejercicio13.py
@@ -0,0 +1,8 @@
+from operator import mod
+
+
+Ano= float( input("Ingresar año: "))
+if (Ano % 4) and (Ano % 100<>0) or (Ano % 400=0) 1000:
+    print ("El año es BISIESTO")
+    else:
+        print ("El año no es BISIESTO")
